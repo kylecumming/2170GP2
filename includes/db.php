7cc513f2515665d6110d26feaf532760a0ad54e3
Ben Lee <bn628547@dal.ca>
2021-04-08 17:15:24 -0300

    Set db.php for dal server

--- includes/db.php
+++ includes/db.php
@@ -2,10 +2,11 @@
 	//Database connection script
 	//Connecting to 2170db database
 	//Username and Password will need to be modified to meet local setup
-	$hostservername = "localhost";
-	$username = "root";
-	$password = "root";
-	$dbname = "jediDB";
+	$hostservername = "db.cs.dal.ca";
+    $username = "cumming";
+    $password = "r25iTVnLv8FNKazUydDUTucrE";
+    $dbname = "cumming";
+
 
 	$mysqli = new mysqli($hostservername, $username, $password, $dbname);
 
@@ -14,7 +15,7 @@
 	{
 		die("Failed to connect to mysql: ($mysqli->connect_error) $mysqli->connect_error");
 	} else {
-    	echo "DB Connected! (Remove after development)";
+    	//echo "DB Connected! (Remove after development)";
     	//Connected statement can be un commented for debugging
 	}
 ?>
