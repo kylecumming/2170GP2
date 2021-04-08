692a67d3d7d368745593d4014a72f3b51716d981
Keaton Gibb <kt224201@dal.ca>
2021-04-08 17:25:41 -0300

    Update logout.php

--- includes/logout.php
+++ includes/logout.php
@@ -1,3 +1,6 @@
+<!-- Added ability to log out of session: Keaton Gibb B00833276
+This really helped me know how to use $_POST better and $_SESSION which helped me with my individual assignments-->
+
 <?php
 session_start();
 if (session_destroy()) {
